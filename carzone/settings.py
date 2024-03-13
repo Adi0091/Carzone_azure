986c46f497391b52d458a33f21584cc3a4888eac
Adi0091 <waghayeadi@gmail.com>
2024-03-13 21:10:58 +0530

    completed search functionalities and admin customization

--- carzone/settings.py
+++ carzone/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- carzone/urls.py
+++ carzone/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('cars/', include('cars.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
